db4c0f52ba5a9dd1e785b0b0d93889063695da1c
mohamadmahditavakoli <mohamadmahditavakoli8@gmail.com>
2024-06-09 01:04:42 +0330

    Add project files.

---
+++ CRUD_Dapper/Controllers/CustomerController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repositories;
+
+namespace CRUD_Dapper.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+        public IActionResult Index()
+        {
+            return View(_customerRepository.GetCustomers());
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(_customerRepository.Find(id));
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CustomerDto customer)
+        {
+            try
+            {
+                _customerRepository.Add(customer);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(_customerRepository.Find(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(CustomerDto customer)
+        {
+            try
+            {
+                _customerRepository.Update(customer);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(_customerRepository.Find(id));
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(CustomerDto customer)
+        {
+            try
+            {
+                _customerRepository.Delete(customer.Id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
